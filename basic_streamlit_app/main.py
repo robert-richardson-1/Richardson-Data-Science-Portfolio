85de116a51de707672180411c44bf447477fdaac
robert-richardson-1 <rrichar5@nd.edu>
2026-02-08 20:09:56 -0500

    update titles and add more filters

--- basic_streamlit_app/main.py
+++ basic_streamlit_app/main.py
@@ -3,12 +3,16 @@ import streamlit as st
 import pandas as pd
 
 #app title
-st.title("Palmer's Penguins")
+st.title("Palmer's Penguins Data Exploration")
 
 #app description
 st.write("""This app allows the exploration of the **Palmer's Penguins dataset**. 
 Using the interactive features, you can filter the penguin data by species, 
 island, and body mass to see how different penguins compare!""")
+
+st.write("""By adjusting both the selectors, sliders, and physical ordering commands
+by clicking on the physical dataset, you can filter the data in
+various ways.""")
 
 #load data
 @st.cache_data
@@ -17,9 +21,9 @@ def load_data():
 df = load_data()
 
 #filters
-st.sidebar.header("Filter Penguins")
+st.sidebar.header("Interactive Filters:")
 
-#selection, island, and mass
+#select species, island, bill length, bill depth, flipper length, body mass, sex, and year
 species = st.sidebar.multiselect(
     "Select Species",
     options=df["species"].unique(),
@@ -32,6 +36,27 @@ island = st.sidebar.multiselect(
     default=df["island"].unique()
 )
 
+bill_length = st.sidebar.slider(
+    "Select Bill Length (mm)",
+    int(df["bill_length_mm"].min()),
+    int(df["bill_length_mm"].max()),
+    (int(df["bill_length_mm"].min()), int(df["bill_length_mm"].max()))
+)
+
+bill_depth = st.sidebar.slider(
+    "Select Bill Length (mm)",
+    int(df["bill_depth_mm"].min()),
+    int(df["bill_depth_mm"].max()),
+    (int(df["bill_depth_mm"].min()), int(df["bill_depth_mm"].max()))
+)
+
+flipper_length = st.sidebar.slider(
+    "Select Flipper Length (mm)",
+    int(df["flipper_length_mm"].min()),
+    int(df["flipper_length_mm"].max()),
+    (int(df["flipper_length_mm"].min()), int(df["flipper_length_mm"].max()))
+)
+
 body_mass = st.sidebar.slider(
     "Select Body Mass Range (g)",
     int(df["body_mass_g"].min()),
@@ -39,22 +64,18 @@ body_mass = st.sidebar.slider(
     (int(df["body_mass_g"].min()), int(df["body_mass_g"].max()))
 )
 
-iltered_df = df[
-    (df["species"].isin(species)) &
-    (df["island"].isin(island)) &
-    (df["body_mass_g"].between(body_mass[0], body_mass[1]))
-]
-
-
 filtered_df = df[
     (df["species"].isin(species)) &
     (df["island"].isin(island)) &
+    (df["bill_length_mm"].between(bill_length[0], bill_length[1])) &
+    (df["bill_depth_mm"].between(bill_depth[0], bill_depth[1])) &
+    (df["flipper_length_mm"].between(flipper_length[0], flipper_length[1])) &
     (df["body_mass_g"].between(body_mass[0], body_mass[1]))
 ]
 
-#display data (with filters if they have it)
-st.subheader("Filtered Penguin Data")
-st.write(f"Showing {filtered_df.shape[0]} penguins")
+#display data (with filters if inputed)
+st.subheader("Interactive Filtered Penguin Data")
+st.write(f"Based on selected inputs, showing {filtered_df.shape[0]} penguins")
 
 st.dataframe(filtered_df)
 
